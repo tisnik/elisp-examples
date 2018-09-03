d80345660f26574204b8aa37c41850c7924f46c0
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-09-03 09:17:20 +0200

    Examplke #1: Global variables

---
+++ elisp-2/01_varibles.el
@@ -0,0 +1,28 @@
+(set 'zero 0)
+(print zero)
+
+(setq answer 42)
+(print answer)
+
+(setq answer 43)
+(print answer)
+
+(setq x 10
+      y 20)
+(print x)
+(print y)
+
+(defconst pi 3.141592653589793)
+(print pi)
+
+(defconst pi 3)
+(print pi)
+
+(defvar e 2.7172)
+(print e)
+
+(defvar e 5.55)
+(print e)
+
+(defvar delta 4.669201609102990671853203821)
+(print delta)
