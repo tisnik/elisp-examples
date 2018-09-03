89d2dfb0bc0eb7653754a4f8261f4944dd7c7a8a
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-09-03 09:17:39 +0200

    Example #2: Global variables and docstrings

---
+++ elisp-2/02_variable_docstrings.el
@@ -0,0 +1,17 @@
+(defconst pi-float 3.141592653589793 "Ratio of a circle's circumference to its diameter")
+
+(print pi-float)
+(print (describe-variable 'pi-float))
+
+(print (describe-variable 'float-pi))
+
+
+(defvar delta 4.669201609102990671853203821 "Feigenbaum constant")
+
+(print delta)
+(print (describe-variable 'delta))
+
+
+(defvar unknown nil)
+(print unknown)
+(print (describe-variable 'unknown))
