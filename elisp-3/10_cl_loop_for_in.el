156597f8379485d47cff2298c58d340e5b95fe66
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-09-10 12:06:31 +0200

    Example #10: cl-loop macro - basic for-each loop

---
+++ elisp-3/10_cl_loop_for_in.el
@@ -0,0 +1,22 @@
+(require 'cl-lib)
+
+(setq lst (number-sequence 0 10))
+
+(print
+    (cl-loop for i in lst
+             collect i))
+
+(print
+    (cl-loop for i in lst
+             collect (* i i)))
+
+
+(defun factorial (n)
+  (setq accumulator 1)
+  (dolist (value (number-sequence 1 n))
+    (setq accumulator (* accumulator value)))
+  accumulator)
+
+(print
+    (cl-loop for i in lst
+             collect (factorial i)))
