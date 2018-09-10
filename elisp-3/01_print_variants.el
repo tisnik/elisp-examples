c8bd28335287420e11e90c635cd6f21647c17759
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-09-10 12:00:48 +0200

    Example #1: print function variants

---
+++ elisp-3/01_print_variants.el
@@ -0,0 +1,26 @@
+(print 42)
+(print "Hello")
+(print :world)
+(print '(1 2 3))
+
+(print "-----------------------------------------")
+
+(prin1 42)
+(terpri)
+(prin1 "Hello")
+(terpri)
+(prin1 :world)
+(terpri)
+(prin1 '(1 2 3))
+(terpri)
+
+(print "-----------------------------------------")
+
+(princ 42)
+(terpri)
+(princ "Hello")
+(terpri)
+(princ :world)
+(terpri)
+(princ '(1 2 3))
+(terpri)
