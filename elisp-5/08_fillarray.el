cb54a1e2c880f34fa2f3039796a1721ffa89771c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-09-19 11:14:39 +0200

    Example #8: fillarray function

---
+++ elisp-5/08_fillarray.el
@@ -0,0 +1,14 @@
+(setq v1 [1 2 3 4])
+(setq str1 "Hello")
+
+(print v1)
+(print str1)
+
+(print "-----------------------------------------")
+
+(fillarray v1 0)
+(fillarray str1 ?*)
+
+(print v1)
+(print str1)
+
